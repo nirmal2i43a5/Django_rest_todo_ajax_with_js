d76eb0edbff8fa71b0525cfbcb4ad6b81c539b0f
Nirmal <nirmalpandey048@gmail.com>
2020-11-13 14:36:50 +0545

    added cors settings

--- project_api/settings.py
+++ project_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'rest_framework',
     'api_app',
     'frontend',
@@ -84,7 +85,7 @@ DATABASES = {
     }
 }
 
-
+    
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
